36431252e47e5814cad123c96a23bae6c019b4e9
Mann Gupta <manng@umich.edu>
2023-12-28 10:25:03 +0100

    made navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 import { Inter, Pixelify_Sans } from 'next/font/google'
 
@@ -8,6 +9,7 @@ const pixelify_sans = Pixelify_Sans({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#202020]">
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
         <Header />
       </div>
