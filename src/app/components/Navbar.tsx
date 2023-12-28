111714072f25a7c09c54aa32fc664c6efe6ce7a8
Mann Gupta <manng@umich.edu>
2023-12-28 11:12:59 +0100

    made NavLink component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,18 +1,39 @@
 import Link from 'next/link'
 import React from 'react'
+import NavLink from './NavLink'
+import { link } from 'fs';
+
+const navLinks = [
+    {
+        title: "About",
+        path: "#about",
+    },
+    {
+        title: "Projects",
+        path: "#projects",
+    },
+    {
+        title: "Contact",
+        path: "#contact",
+    }
+];
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-            <Link href="/" className="text-5xl text-white font-semibold">manng</Link>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#202020] bg-opacity-90">
+        <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+            <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">
+                manng
+            </Link>
             <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul>
-                    <li>
-                        <Link href={"#about"} className="block py-2 pl-3 pr-4 text-gray-400 sm:text-xl rounded md:p-0 hover:text-white">
-                        About
-                        </Link>
-                    </li>
+                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                    {
+                        navLinks.map((link,index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
@@ -20,4 +41,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#202020]">
       <Navbar />
-      <div className="container mx-auto px-12 py-4">
+      <div className="container mt-24 mx-auto px-12 py-4">
         <Header />
       </div>
     </main>
