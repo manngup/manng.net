950a1fa63b842c16d926d0cd27f35161efb069fb
Mann Gupta <manng@umich.edu>
2023-12-28 10:12:45 +0100

    edited header section text

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    Mann Gupta
+                    Hi I'm Mann!
                     <br />
                     <TypedName />
                 </h1>
