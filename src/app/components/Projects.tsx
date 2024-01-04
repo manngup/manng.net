e1b0a81bdfe619fe83119b381ced56e44c4b3b84
Mann Gupta <manng@umich.edu>
2024-01-04 13:55:37 -0500

    removed tags attribute for now

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -38,7 +38,7 @@ const Projects = () => {
                     title={project.title}
                     description={project.description}
                     imgURL={project.img}
-                    tags={project}
+                    //tags={project}
                 />
             ))}
         </div>
