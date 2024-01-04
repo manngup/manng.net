40d2ba190b30fc335d2e95f48de0f358315016e0
Mann Gupta <manng@umich.edu>
2024-01-04 17:15:16 -0500

    added scale up effect to ProjectCard

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -13,12 +13,12 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ imgURL, title, description, gitURL, launchURL }) => {
   return (
-    <div>
+    <div className="hover:scale-105 transition-all duration-500">
         <div 
             className="h-52 md:h-72 rounded-t-lg relative group"
             style={{ background: `url(${imgURL})`, backgroundSize: "cover"}}
         >
-          <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          <div className="overlay rounded-t-lg items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
             <Link href={gitURL} className="h-14 w-14 mr-2 border-2 rounded-full border-gray-400 hover:border-white relative group/link">
               <GitHubIcon className="h-10 w-10 text-gray-400 cursor-pointer group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
             </Link>
