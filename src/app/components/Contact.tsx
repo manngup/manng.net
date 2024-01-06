c65dcc45b23e12e063f0be1d264ac537d4f696ce
Mann Gupta <manng@umich.edu>
2024-01-06 13:31:21 -0500

    added frontend of message form in Contact

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const Contact = () => {
   return (
-    <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+    <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">        
         <div>
             <h5 className="text-xl font-bold text-white my-2">
                 Connect with me!
@@ -23,8 +23,46 @@ const Contact = () => {
             </div>
         </div>
         <div>
-            <form className="flex flex-col gap-4">
-                space for email contact form
+            <form className="flex flex-col">
+                <div className="mb-6">
+                    <label htmlFor="email" className="text-white block text-sm font-medium mb-2">
+                        Your Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        required placeholder="name@gmail.com"
+                        className="bg-gray-100 border border-gray-400 placeholder-gray-400 text-gray-600 rounded p-2 block w-full"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="subject" className="text-white block text-sm font-medium mb-2">
+                        Subject
+                    </label>
+                    <input
+                        type="text"
+                        id="subject"
+                        required placeholder="Hi, what's up!"
+                        className="bg-gray-100 border border-gray-400 placeholder-gray-400 text-gray-600 rounded p-2 block w-full"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="message" className="text-white block text-sm font-medium mb-2">
+                        Message
+                    </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        required placeholder="Are you free sometime to chat?"
+                        className="bg-gray-100 border border-gray-400 placeholder-gray-400 text-gray-600 rounded p-2 block w-full"
+                    />
+                </div>
+                <button 
+                    type="submit"
+                    className="border rounded block w-full p-3 font-semibold hover:scale-105 transition-all duration-250"
+                >
+                    Send Message
+                </button>
             </form>
         </div>
     </section>
